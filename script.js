b6e87b82bfa2f12215047ab5107b71e925ce55b4
muhammet mümin bay <89353370+MuminBay@users.noreply.github.com>
2024-08-20 11:46:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const liste = document.querySelector("ul")
 
 
 window.onload = function () {
-   if (!sessionStorage.getItem('pageRefreshed')){
-       localStorage.clear();
-   }
-
-   sessionStorage.setItem('pageRefreshed', 'true');
-
+    if (!sessionStorage.getItem('pageRefreshed')) {
+        localStorage.clear();
+    }
 
     let items = JSON.parse(localStorage.getItem('items')) || [];
+    items = items.filter(item => item.text.trim() !== "");
+    localStorage.setItem('items', JSON.stringify(items));
+
     items.forEach(function (item) {
         addListitem(item);
     });
+
+    sessionStorage.setItem('pageRefreshed', 'true');
 };
 
 
